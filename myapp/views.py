704ef46d15949da187268875b252a31d17df1bb8
cheeto <dakejiang123@163.com>
2023-12-28 20:32:16 +0800

    feat(main): temlate init

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def index(request):
+    print(request, 'request ------------')
+    return HttpResponse("欢迎使用")
+
+
+def user_list(request):
+    print(request, 'user list request -------')
+    return HttpResponse("用户列表")
+
+
+def user_add(request):
+    print(request, 'user add request -------')
+    # 优先去根目录下templates中寻找，要在settings中配置
+    # 根据app注册顺序寻找
+    # 去当前app下寻找templates寻找user_add.html
+    return render(request, 'user_add.html')
